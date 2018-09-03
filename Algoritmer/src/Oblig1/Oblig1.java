c449cbbf09e67bcec4c4804be75ba4e32a82a7b5
M. Furkan Ergin <furkann98@hotmail.com>
2018-09-03 13:05:44 +0200

    Oppgave 1 - Maks()

---
+++ Algoritmer/src/Oblig1/Oblig1.java
@@ -0,0 +1,50 @@
+package Oblig1;
+
+public class Oblig1 {
+
+    public static void main(String[] args){
+        int[] test = randomArray(16);
+
+        for( int i : test){
+            System.out.print(i + ", ");
+        }
+        System.out.println();
+        System.out.println(maks(test));
+    };
+
+    public static int[] randomArray(int n){
+      int[] Array = new int[n];
+
+      for(int i = 0; i < Array.length; i++){
+        Array[i] = i+1;
+      }
+
+      for(int i = Array.length-1; i > 0; i--){
+          int r = (int) (Math.random()*i) ;
+          int tmp = Array[i];
+          Array[i] = Array[r];
+          Array[r] = tmp;
+      }
+
+      return Array;
+    };
+
+    public static int maks(int[] a){
+        int maks = a[0];
+
+        for(int i = 0; i < a.length - 1; i++){
+           if(a[i] > a[i+1]){
+                int tmp = a[i];
+                a[i] = a[i+1];
+                a[i+1] = tmp;
+
+                maks = tmp;
+
+            }
+        }
+        for( int i : a){
+            System.out.print(i + ", ");
+        }
+        return maks;
+    };
+}
--- Algoritmer/src/com/company/Uke2.java
+++ Algoritmer/src/com/company/Uke2.java
@@ -3,7 +3,47 @@ package com.company;
 public class Uke2 {
 
     public static void main(String[] args){
+        int[] a = {0,1,2,3,4,5};
+        int[] b = {5,1,8,3,0,2};
+
+        int min = finnMin(b,0,a.length);
+        System.out.println(min);
+
+        String tekst = skrivUtArray(a);
+        System.out.println(tekst);
+
+
 
     }
-    
+
+
+    //Oppgave 1.2.1
+    //1
+    public static int finnMin(int[] a, int fra, int til){
+
+        int indeks = fra;
+        int minVerdi = a[fra];
+
+        for(int i = fra+1; i < til; i++ ){
+            if(minVerdi > a[i]){
+                indeks = i;
+                minVerdi = a[i];
+            }
+        }
+
+        return indeks;
+    };
+
+    //2
+    public static String skrivUtArray(int[] a){
+        String tekst = "[ ";
+
+        for(int i : a){
+            tekst = tekst + i + ", ";
+        }
+        tekst = tekst + " ]";
+
+        return tekst;
+    };
+
 }
