d0e751271f9a7695e7999a47c20217cbb80aa446
M. Furkan Ergin <furkann98@hotmail.com>
2018-09-04 11:38:19 +0200

    Glem dette

--- Algoritmer/src/Oblig1/Oblig1.java
+++ Algoritmer/src/Oblig1/Oblig1.java
@@ -1,16 +1,24 @@
 package Oblig1;
 
+import java.util.NoSuchElementException;
+
 public class Oblig1 {
 
     public static void main(String[] args){
-        int[] test = randomArray(50);
+
+        int[] a = {1, 3, 7, 5, 2, 9};
+
+        System.out.println(ombyttinger(a));
+        /*int[] test = randomArray(10);
 
         for( int i : test){
             System.out.print(i + ", ");
         }
         System.out.println();
-        System.out.println("Største tallet  i arrayen er: " + maks(test));
-    };
+        System.out.println("Største tallet  i arrayen er: " + ombyttinger(test));
+        */
+
+    }
 
     public static int[] randomArray(int n){
       int[] Array = new int[n];
@@ -30,6 +38,8 @@ public class Oblig1 {
     };
 
     public static int maks(int[] a){
+        if(a.length == 0) throw new NoSuchElementException("Arrayen er tom");
+
         int maks = a[0];
 
         for(int i = 0; i < a.length - 1; i++){
@@ -47,5 +57,24 @@ public class Oblig1 {
         }
         System.out.println();
         return maks;
-    };
+    }
+
+    public static int ombyttinger(int[] a){
+        if(a.length == 0) throw new NoSuchElementException("Arrayen er tom");
+
+        int antall = 0;
+
+        for(int i = 0; i < a.length - 1; i++){
+            if(a[i] > a[i+1]){
+                int tmp = a[i];
+                a[i] = a[i+1];
+                a[i+1] = tmp;
+
+                antall++;
+
+            }
+        }
+        return antall;
+
+    }
 }
