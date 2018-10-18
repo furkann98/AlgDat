aeed4b548154c3518b0319e6e260ffa8eb981352
PedramRjoo <Pedi-r@hotmail.com>
2018-10-18 17:18:28 +0200

    Oppgave 1, 2, 3 og 4 BIIISH <3

--- Algoritmer/src/Oblig3/Oblig3Test.java
+++ Algoritmer/src/Oblig3/Oblig3Test.java
@@ -30,12 +30,12 @@ public class Oblig3Test
     antallFeil += oppgave2();
     antallFeil += oppgave3();
     antallFeil += oppgave4();
-    antallFeil += oppgave5();
-    antallFeil += oppgave6();
-    antallFeil += oppgave7();
-    antallFeil += oppgave8();
-    antallFeil += oppgave9();
-    antallFeil += oppgave10();
+    //antallFeil += oppgave5();
+    //antallFeil += oppgave6();
+   // antallFeil += oppgave7();
+    //antallFeil += oppgave8();
+    //antallFeil += oppgave9();
+    //antallFeil += oppgave10();
 
         if (antallFeil == 0)
         {
--- Algoritmer/src/Oblig3/ObligSBinTre.java
+++ Algoritmer/src/Oblig3/ObligSBinTre.java
@@ -46,7 +46,37 @@ public class ObligSBinTre<T> implements Beholder<T>
     @Override
     public boolean leggInn(T verdi)
     {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+        Objects.requireNonNull(verdi, "Ulovlig med nullverdier!");
+
+       if(tom()){
+           rot = new Node<T>(verdi,null);
+       }
+
+       else {
+           Node<T> p = rot, q = null;               // p starter i roten
+           int cmp = 0;                             // hjelpevariabel
+
+           while (p != null)                        // fortsetter til p er ute av treet
+           {
+               q = p;                                 // q er forelder til p
+               cmp = comp.compare(verdi, p.verdi);     // bruker komparatoren
+               p = cmp < 0 ? p.venstre : p.høyre;     // flytter p
+           }
+
+           // p er nå null, dvs. ute av treet, q er den siste vi passerte
+
+           p = new Node<>(verdi,q);                  // oppretter en ny node
+
+           if (q == null) rot = p;                  // p blir rotnode
+           else if (cmp < 0) q.venstre = p;         // venstre barn til q
+           else q.høyre = p;                        // høyre barn til q
+
+
+       }
+
+        antall++;
+        endringer++;// én verdi mer i treet
+        return true;
     }
 
     @Override
@@ -86,7 +116,25 @@ public class ObligSBinTre<T> implements Beholder<T>
 
     public int antall(T verdi)
     {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+        if(verdi.equals(null))return 0;
+
+
+        int antallLike = 0;
+        Node<T> p = rot;
+
+        while( p!= null){
+            int cmp = comp.compare(verdi, p.verdi);
+            if(cmp<0)p=p.venstre;
+            else{
+                if(cmp==0) antallLike++;
+                p = p.høyre;
+            }
+
+        }
+        return antallLike;
+
+
+       // throw new UnsupportedOperationException("Ikke kodet ennå!");
     }
 
     @Override
@@ -103,18 +151,152 @@ public class ObligSBinTre<T> implements Beholder<T>
 
     private static <T> Node<T> nesteInorden(Node<T> p)
     {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+      if(p.høyre !=null){
+           p = p.høyre;
+           while(p.venstre != null){
+               p = p.venstre;
+           }
+           return p;
+       }else{
+           while(p!=null){
+               if(p.forelder!=null && p.forelder.høyre != p){
+                   return p.forelder;
+               }
+               p = p.forelder;
+           }
+       }
+       return p;
+
+    /*
+        if(p.høyre == null && p.venstre == null) return null;
+         if(p.høyre !=null){
+            p = p.høyre;
+            while(p.venstre != null && p.forelder.høyre != p){
+                p = p.venstre;
+            }
+             return p.forelder;
+        }
+        else if(p.høyre == null && p.venstre!= null){
+            return p.forelder;
+        }
+        return p; */
+
+        // throw new UnsupportedOperationException("Ikke kodet ennå!");
     }
 
     @Override
     public String toString()
     {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+       if(tom()) return "[]";
+
+
+       StringBuilder sb = new StringBuilder();
+       sb.append("[");
+
+       Node<T> p = rot;
+       while(p.venstre != null){
+           p = p.venstre;
+       }
+
+       for(int i = 0; i < antall; i++){
+           sb.append(p.verdi);
+           if(i != (antall-1)) sb.append(", ");
+           p = nesteInorden(p);
+       }
+
+       sb.append("]");
+        //System.out.println(sb.toString());
+
+
+        return sb.toString();
+       /*
+      else if(antall == 1) return"[" + rot.verdi + "]";
+
+
+       String tekst = "[";
+
+        Node<T> p=  rot;
+
+        while(p.venstre != null){
+            p = p.venstre;
+        }
+
+        tekst += p.verdi;
+        p = nesteInorden(p);
+
+        while(nesteInorden(p)!= null){
+            tekst += ", " + p.verdi;
+            p = nesteInorden(p);
+        }
+
+        tekst += "]";
+
+        return tekst;
+*/
+
+      /* if(antall < 1){ return  "[]";}
+        //else if(tom()){return"[]";}
+
+       String tekst = "[";
+       Node<T> p = rot;
+
+       for (;p!=null;){
+           p = p.venstre;
+       }
+
+
+       tekst += p.verdi;
+       p = nesteInorden(p);
+
+
+       for(int i = 1;i<antall;i++){
+           tekst += "," + p.verdi;
+           p = nesteInorden(p);
+       }
+
+
+       tekst += "]";
+       return tekst;
+*/
+
+
+      //  throw new UnsupportedOperationException("Ikke kodet ennå!");
     }
 
     public String omvendtString()
     {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+        if(tom()) return "[]";
+        StringBuilder sb = new StringBuilder();
+        Deque<Node> stack = new ArrayDeque<>();
+        sb.append("[");
+
+        Node<T> p = rot;
+
+        while(p.venstre != null){
+            p = p.venstre;
+        }
+
+        for(int i = 0; i < antall; i++){
+            stack.addFirst(p);
+            p = nesteInorden(p);
+           // System.out.println("STACK " +  stack.pop());
+        }
+
+
+        for(int i = 0; i< antall; i++){
+            sb.append(stack.pop());
+            if(i!=(antall-1)) sb.append(", ");
+            //stack.removeFirst();
+        }
+
+        sb.append("]");
+        //System.out.println(sb.toString());
+
+        return sb.toString();
+
+
+
+        //throw new UnsupportedOperationException("Ikke kodet ennå!");
     }
 
     public String høyreGren()
